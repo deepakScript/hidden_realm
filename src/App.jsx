06dbb381f59a2944290329443b32311eaf0b5683
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-10-15 12:46:06 +0545

    feat:about

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/About'
+import Navbar from './components/Navbar'
 
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'>
       <Hero/>
+      <About/>
+      <Navbar/>
     </main>
   )
 }
